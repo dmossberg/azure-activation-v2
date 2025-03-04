9eac8575abbfcd02fbe4e5de94149369bec47b3f
Daniel Mossberg <daniel.mossberg@dynatrace.com>
2025-03-04 17:50:07 +0100

    Second attempt

---
+++ update-monitoring-configuration.ps1
@@ -0,0 +1,39 @@
+$environment = "$($Env:dynatraceEnvironment ?? 'dev')"
+#$endpoint = "https://$Env:dynatraceTenant.$environment.dynatracelabs.com/api/v2/extensions/azure/monitoringConfigurations"
+$endpoint = "https://webhook.site/67fc63f3-ff55-4154-ac1c-78c1b1d206d3"
+
+$jsonBody = @"
+{
+  "enabled": true,
+  "description": "some_configuration",
+  "version": "0.0.1",
+  "featureSets": [],
+  "azure": {
+    "credentials": [
+      {
+        "description": "New Account",
+        "enabled": true,
+        "connectionId": "$($Env:connectionId)"
+      }
+    ]
+  }
+}
+"@
+
+$response = Invoke-WebRequest -SkipCertificateCheck `
+    -ContentType "application/json" `
+    -Method POST `
+    -Uri $endpoint `
+    -Headers @{
+        "Accept" = "application/json"
+        "Authorization" = "Api-Token $Env:dynatraceApiKey"
+    } `
+    -Body ($jsonBody)
+
+if ($response.StatusCode -eq 200) {
+    $jsonResponse = $response.Content | ConvertFrom-Json
+    $DeploymentScriptOutputs = @{}
+    $DeploymentScriptOutputs['hyperscalerAuthServiceObjectId'] = $jsonResponse[0].objectId
+} else {
+    throw "Request failed with status code: $($response)"
+}
